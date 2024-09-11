1aa2bc8dcca91d7c5b87368fcc6c73bbaa59c1b2
Srishti Harit <86703642+haritsrishti@users.noreply.github.com>
2024-09-11 16:34:43 +0530

    Add files via upload

---
+++ ODA/src/test/java/com/appointmentSystem/ODA/OdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appointmentSystem.ODA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
